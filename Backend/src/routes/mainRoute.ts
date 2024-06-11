cb8958b18cd6b38d8469d2c2116af1987143826c
TirthPatel077 <tirthgentleman1234@gmail.com>
2024-06-11 14:05:57 +0530

    v

--- Backend/src/routes/mainRoute.ts
+++ Backend/src/routes/mainRoute.ts
@@ -4,15 +4,14 @@ import chatRouter from './chatRoutes.js';
 import applicationRouter from './applicationRoutes.js';
 // import Question from '../models/Question.js';
 import Questions from './Questions_routes.js';
+import Tests from './Testroutes.js';
 
 const router = Router()
 
 router.use("/user", userRouter);
 router.use("/chat", chatRouter);
 router.use("/application", applicationRouter);
-<<<<<<< HEAD
 router.use("/question",Questions);
+router.use("/test",Tests);
 
-=======
->>>>>>> a86b54fcc10c8ea6e09595bc87a7045a98ef80fd
 export default router
